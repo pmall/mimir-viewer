1a32bcf32ef522ab83a493a1ed49adfe5c2465c7
pmall <pierre.mallinjoud@gmail.com>
2026-02-25 21:54:22 +0100

    feat: setup database connection with drizzle orm

---
+++ src/db/schema.ts
@@ -0,0 +1,15 @@
+import {
+  pgTable,
+  serial,
+  text,
+  real,
+  boolean,
+  integer,
+  json,
+} from "drizzle-orm/pg-core";
+
+// Define your tables down here! For example:
+// export const users = pgTable("users", {
+//   id: serial("id").primaryKey(),
+//   name: text("name").notNull(),
+// });
